e6572225f5691fc070190b617d8d2842a0e25bbe
bibo0101 <141605919+bibo0101@users.noreply.github.com>
2025-01-31 16:25:55 +0530

    Update testcases.tsx

--- testcases.tsx
+++ testcases.tsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import AttestationPanel from './AttestationPanel';
+import { updateFcrmAttestation } from '../../../redux/slices/fcrmAttestationSlice';
+import { getUserInfo } from '../../helpers/userDetails';
+jest.mock('react-redux', () => ({
+    useSelector: jest.fn(),
+    useDispatch: jest.fn(),
+}));
+
+jest.mock('../../../redux/slices/fcrmAttestationSlice', () => ({
+    updateFcrmAttestation: jest.fn(),
+}));
+
+jest.mock('../../helpers/userDetails', () => ({
+    getUserInfo: jest.fn(),
+}));
 
